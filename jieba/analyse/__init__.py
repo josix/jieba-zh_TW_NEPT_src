06be8586d021f9188defe5bf42ec6e2d521d0134
WilsonWang <wilson8507@gmail.com>
2018-11-07 01:55:23 +0800

    Add textrank vairants

--- jieba/analyse/__init__.py
+++ jieba/analyse/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from .tfidf import TFIDF
 from .textrank import TextRank
+from .textrank_similarity import TextRankSimilarity
+from .textrank_vsm import TextRankVSM
 try:
     from .analyzer import ChineseAnalyzer
 except ImportError:
@@ -8,10 +10,14 @@ except ImportError:
 
 default_tfidf = TFIDF()
 default_textrank = TextRank()
+default_textrank_similarity = TextRankSimilarity()
+default_textrank_vsm = TextRankVSM()
 
 extract_tags = tfidf = default_tfidf.extract_tags
 set_idf_path = default_tfidf.set_idf_path
 textrank = default_textrank.extract_tags
+textrank_similarity = default_textrank_similarity.extract_tags
+textrank_vsm = default_textrank_vsm.extract_tags
 
 def set_stop_words(stop_words_path):
     default_tfidf.set_stop_words(stop_words_path)
